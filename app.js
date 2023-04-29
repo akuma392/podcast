837967244284b8e394939e370a4fdb695304a08a
Abhishek <abhivision3@gmail.com>
2023-04-29 12:44:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -40,7 +40,8 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(express.cookieParser('secret'));
+app.use(express.cookieSession());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(
