e89b0ab96eda4de56febb8de9088bf546e3bffce
Abhishek Kumar <abhivision3@gmail.com>
2023-07-08 19:04:33 +0530

    updated endpoint

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var mongoose = require('mongoose');
-var auth = require('./middlewares/auth');
+var mongoose = require("mongoose");
+var auth = require("./middlewares/auth");
 
-var session = require('express-session');
-var MongoStore = require('connect-mongo')(session);
-var flash = require('connect-flash');
+var session = require("express-session");
+var MongoStore = require("connect-mongo")(session);
+var flash = require("connect-flash");
 
-require('dotenv').config();
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var podcastRouter = require('./routes/podcasts');
+require("dotenv").config();
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var podcastRouter = require("./routes/podcasts");
 
 // mongoose.connect(
 //   process.env.MONGO_DB,
@@ -24,25 +24,24 @@ var podcastRouter = require('./routes/podcasts');
 //   }
 // );
 mongoose.connect(
-  'mongodb+srv://abhi:abhi1234@cluster0.u6ogw.mongodb.net/podcast?retryWrites=true&w=majority',
+  "mongodb+srv://abhi:abhi1234@cluster0.u6ogw.mongodb.net/podcast?retryWrites=true&w=majority",
   { useNewUrlParser: true, useUnifiedTopology: true },
   (err) => {
-    console.log(err ? err : 'Connected to database');
+    console.log(err ? err : "Connected to database");
   }
 );
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.cookieParser('secret'));
-app.use(express.cookieSession());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(
   session({
@@ -56,9 +55,9 @@ app.use(flash());
 
 app.use(auth.userInfo);
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/podcasts', podcastRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/podcasts", podcastRouter);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
@@ -68,11 +67,11 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
